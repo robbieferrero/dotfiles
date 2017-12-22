0cb4ff112c778142ecb31d3d080fdf3936d2b7a0
Robbie Ferrero <robbieferrero@gmail.com>
2017-12-22 11:26:02 -0600

    Adding linux install basics

---
+++ linux-install-cli-tools.sh
@@ -0,0 +1,14 @@
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
+sudo apt-get -y autoremove
+
+sudo apt-get install -y build-essential cmake
+
+sudo apt-get install -y unzip
+
+sudo apt-get install -y faketime htop lshw pdftk wget
+
+sudo apt-get install -y git
+curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+sudo apt-get install -y nodejs
